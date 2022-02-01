b63daf2510463b0a586615deadf4a76a87d8fc3e
sssssssssswag <n00201066@iadt.ie>
2022-02-01 15:15:20 +0000

    :D

---
+++ p5Starter-main/bleah.js
@@ -0,0 +1,22 @@
+let distance= 5
+
+String "dickhead"
+Float 2
+int 3
+Boolean false
+Array[]
+
+Object()
+
+
+doSomething
+// ^ VARIABLE CALL EXAMPLE
+someFunc()
+// ^ FUNCTION/ METHOD CALL EXAMPLE
+
+function someFunc(){
+    // ^ FUNCTION DECLARATION/ INITIALISATION
+    ebuj;
+}
+
+//yfvfylfyilyil
--- script.js
+++
@@ -1,9 +0,0 @@
-let distance= 5
-
-String "dickhead"
-Float 2
-int 3
-Boolean false
-Array[]
-
-Object()
