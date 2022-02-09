3d1eae87bcc39316c9a5a765d729fcf2b9740b81
swagggggggggg <n00201066@iadt.ie>
2022-02-09 13:34:14 +0000

    day 02 changelog- remapped data values to scale to the top of the graph and updated checks with math to reflect that. also added dataLabels for future integration

--- Week02 - Bar Chart/sketch.js
+++ Week02 - Bar Chart/sketch.js
@@ -7,8 +7,12 @@ let margin= 20;
 let spacing= 10;
 let numChecks= 10;
 let checkSpacing= chartW/numChecks;
+let checkIncrements;
+let labelSpacing= 5;
 
-let data = [50, 110, 270, 150, 50, 10, 15, 30, 60, 5, 40, 60, 80, 100, 120];
+let data        = [50, 110, 200, 150, 50, 40, 60, 80, 100, 120, 30, 80];
+let dataLabels  = ["d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "d", "a", "a"]
+let scaledData= []
 
 let availableWidth= chartW- ((margin*2)+(spacing*(data.length-1)));
 let barWidth= availableWidth/ data.length;
@@ -16,6 +20,14 @@ let barWidth= availableWidth/ data.length;
 function setup(){
     createCanvas(screenW, screenH);
     background(20);
+
+    maxValue = max(data)
+    checkIncrements= Math.round(maxValue/ numChecks);
+
+    for(let i=0; i<data.length; i++){
+        let tempVal= map(data[i], 0, max(data), 0, chartH);
+        scaledData.push(tempVal);
+    }
 }
 
 function draw(){
@@ -40,25 +52,28 @@ function draw(){
         fill(230, 30, 130);
         noStroke();
         textAlign(RIGHT, CENTER);
-        textSize(15);
-        text(k*checkSpacing, -checkSpacing/3, -checkSpacing*k);
+        textSize(12);
+        text(k*checkIncrements, -checkSpacing/3, -checkSpacing*k);
     }
 
     //bars
     translate(margin, 0);
     //translates to margin from the graph's (0, 0) (aggregate translation)
-    for(i= 0; i<data.length; i++){
-        // epilepsy fill (not good!)
-        // fill(random(10, 255), random(10, 255), random(10, 255));
-
+    for(i= 0; i<scaledData.length; i++){
         fill(110, 40+ i*30, 130);
         noStroke();
-        rect((barWidth+ spacing)* i, 0, barWidth, -data[i]);
+        rect((barWidth+ spacing)* i, 0, barWidth, -scaledData[i]);
 
         //text declaring height (AKA data amount)
         fill(0, 200, 130);
         textAlign(CENTER, BOTTOM);
         textSize(15);
-        text(data[i], (barWidth+ spacing)* i + barWidth/2, -data[i]);
+        text(data[i], (barWidth+ spacing)* i + barWidth/2, -scaledData[i]);
+
+        // label text
+        fill(0, 200, 130);
+        textAlign(CENTER, TOP);
+        textSize(15);
+        text(dataLabels[i], (barWidth+ spacing)* i + barWidth/2, labelSpacing);
     }
 }
